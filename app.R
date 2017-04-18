1913dad2bf3a95779e42d3ec10b1756ad213bb58
Andre Duarte <aguimaraesduarte@dons.usfca.edu>
2017-04-17 21:57:18 -0700

    added option to select countries. Makes it slightly less smooth, but worth it

--- app.R
+++ app.R
@@ -1,20 +1,3 @@
-if(!"shiny" %in% rownames(installed.packages())){
-  install.packages("shiny", dependencies = T)
-}
-library("shiny")
-if(!"ggplot2" %in% rownames(installed.packages())){
-  install.packages("ggplot2", dependencies = T)
-}
-library("ggplot2")
-if(!"reshape2" %in% rownames(installed.packages())){
-  install.packages("reshape2", dependencies = T)
-}
-library("reshape2")
-if(!"ggvis" %in% rownames(installed.packages())){
-  install.packages("ggvis", dependencies = T)
-}
-library("ggvis")
-
 # Read data
 df <- read.csv("WorldBankData.csv", stringsAsFactors = F)
 # Remove trailing data (garbage from WDI)
@@ -48,12 +31,13 @@ countries <- sort(as.vector(unique(df$Country)))
 ui <- fluidPage(
   headerPanel("Gapminder Interactive Plot"),
   sidebarPanel(width = 3,
-    selectInput("region", "Select Region", regions),
-    sliderInput("year", "Select Year",
-                min = 1960, max = 2014, value = 1970, sep = "",
-                animate = animationOptions(interval = 100)),
-    sliderInput('pop_size', "Population",
-                min = 500, max = 5000, value = 3000, step = 500, sep = "", ticks = F)
+               selectInput("region", "Select Region", regions),
+               selectizeInput("countries", "Select Countries", countries, multiple = T),
+               sliderInput("year", "Select Year",
+                           min = 1960, max = 2014, value = 1970, sep = "",
+                           animate = animationOptions(interval = 100)),
+               sliderInput('pop_size', "Population",
+                           min = 500, max = 5000, value = 3000, step = 500, sep = "", ticks = F)
   ),
   mainPanel(
     ggvisOutput("plot"),
@@ -63,28 +47,25 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-  sub_df <- reactive({
-    s <- subset(df, Year == input$year, drop=T)
-    s <- subset(s, !is.na(s$LifeExp))
-    s <- subset(s, !is.na(s$Fertility))
+  vis <- reactive({
+    sub_df <- subset(df, Year == input$year, drop=T)
+    sub_df <- subset(sub_df, !is.na(sub_df$LifeExp))
+    sub_df <- subset(sub_df, !is.na(sub_df$Fertility))
     
     region <- input$region
     if(region != "All"){
-      s <- subset(s, Region == region, drop = T)
+      sub_df <- subset(sub_df, Region == region, drop = T)
     }
     
-    return(s)
-  })
-  
-  vis <- reactive({
-    
     popsize <- input$pop_size
+    
+    selected_countries <- input$countries
     
     sub_df %>%
       ggvis(~LifeExp, ~Fertility, fill = ~Region,
             fillOpacity := 0.5, fillOpacity.hover := 1,
             stroke := NA, stroke.hover = ~Region, strokeWidth := 4, strokeOpacity := 0.5) %>%
-      #layer_text(text := ~Country) %>%
+      layer_text(text := ~Country, data = subset(sub_df, Country %in% selected_countries)) %>%
       
       set_options(width = 1000, height = 600, renderer = "svg") %>%
       
@@ -95,7 +76,6 @@ server <- function(input, output) {
       scale_numeric("size", range = c(10, popsize), nice = FALSE) %>%
       
       layer_points(size = ~Population, key := ~Country) %>%
-      #add_legend("size", orient = "left", title="Population") %>%
       hide_legend("size") %>%
       set_options(duration = 0) %>%
       
@@ -113,6 +93,3 @@ server <- function(input, output) {
 }
 
 shinyApp(ui = ui, server = server)
-
-
-
